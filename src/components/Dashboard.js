ce1c9d4a1bf3e28e80b321e7a1435cf4ed2866ba
princerequino <requino.prince@gmail.com>
2022-03-31 15:00:12 -0400

    implement commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
-
-import classnames from "classnames";
+import React, { Component } from 'react';
+import Loading from './Loading';
+import classnames from 'classnames';
+import Panel from './Panel'
 
 const data = [
   {
@@ -35,26 +36,35 @@ const data = [
 
 
 class Dashboard extends Component {
-  state = {
-    loading: false
-  };
+  state = { loading: false, focused: null };
 
+  selectPanel = (id) => {
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
+    }));
+  };
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map(panel => (
-      <Panel
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-      />
-    ));
-
+    const panels = data
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
+      )
+      .map((panel) => (
+        <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.value}
+          onSelect={(event) => this.selectPanel(panel.id)}
+        />
+      ));
     return <main className={dashboardClasses}>{panels}</main>;
   }
 }
